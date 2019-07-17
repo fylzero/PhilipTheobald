f29ded218f9f801785033cb381bfff38700dc671
Philip Theobald <fylzero@gmail.com>
2019-07-16 19:32:23 -0500

    Fresh Laravel Install

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ env('APP_NAME') }}</title>
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    Hello World!
+
+    <div id="app">
+        <example-component></example-component>
+    </div>
+
+    <script src="{{ mix('/js/app.js') }}"></script>
+</body>
+</html>
