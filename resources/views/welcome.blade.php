325a767fd031cb525d257e93174bc5dbe9326945
Philip Theobald <fylzero@gmail.com>
2019-07-19 20:21:38 -0500

    Set meta color

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="theme-color" content="#222222" />
     <title>{{ env('APP_NAME') }}</title>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' rel="stylesheet">
