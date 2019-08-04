a407d8ac82896adbbe45acfbcc367be2f0bf8b4f
Philip Theobald <fylzero@gmail.com>
2019-08-04 17:42:57 -0500

    Added and updated my uses page

--- resources/js/components/Uses.vue
+++ resources/js/components/Uses.vue
@@ -20,6 +20,8 @@
             </picture>
         </div>
 
+        <h1 style="color: #c8dfe6; margin-top: 100px;">Hardware</h1>
+
         <h2>Desk</h2>
         <p><a href="https://www.autonomous.ai/standing-desks/smartdesk-2-business?option1=1&option2=6&option16=36&option17=41" target="blank">SmartDesk 2 Business Edition with a white frame and bamboo top</a> from Autonomous.  Extremely sturdy.  I love that this desk sits at the exact heights I prefer for sitting and standing and it looks beautiful.</p>
 
@@ -29,35 +31,105 @@
         <h2>Monitor</h2>
         <p><a href="https://www.amazon.com/gp/product/B07GWDGVGT/ref=ppx_yo_dt_b_asin_title_o06_s00?ie=UTF8&psc=1" target="blank">Asus VA325H 31.5-Inch LED Monitor</a> which I bought refurbished on Amazon for under $200.  High resolution, not the best color reproduction but good for coding and watching videos.  The monitor stand I use is from Autonomous as well.</p>
 
-        <h2>Audio</h2>
-        <p>I have a pair of <a href="https://www.zzounds.com/item--MACMR824" target="blank">Mackie MR824 Studio Monitors</a>, a <a href="https://www.zzounds.com/item--MACMRS10" target="blank">Mackie MRS10 Powered Studio Subwoofer</a> and a pair of <a href="https://www.amazon.com/Klipsch-ProMedia-Certified-Computer-Speaker/dp/B000062VUO" target="blank">Klipsch Promedia 2.1 Desktop Speakers</a> hooked up with my <a href="https://www.zzounds.com/item--FOCSCAR2I4V2" target="blank">Scarlett Focusrite 2i4 Audio Interface</a>.  I tested tons of studio montor speakers at Guitar Center and landed on these because the low mid and treb frequencies really stood out on the Mackie's to me.  They have a bit of color to the sound but they are very clear and punchy. I use the Klipsch 2.1 speaker set along side the Mackie's to fill out the higher mid range sound for now.  I have the subwwoofer on the Klipsch very low for a bit of top-end lower punch but the Mackie sub is handling all the low end rumble.  Eventually I want to swap out the Klipsch set for some white-face Yamaha monitors.  Likely will do a 6 inch set to drive vocals more.</p>
+        <h2>Speakers</h2>
+        <p>I have a pair of <a href="https://www.zzounds.com/item--MACMR824" target="blank">Mackie MR824 Studio Monitors</a>, a <a href="https://www.zzounds.com/item--MACMRS10" target="blank">Mackie MRS10 Powered Studio Subwoofer</a> and a pair of <a href="https://www.amazon.com/Klipsch-ProMedia-Certified-Computer-Speaker/dp/B000062VUO" target="blank">Klipsch Promedia 2.1 Desktop Speakers</a>.  I tested tons of studio montor speakers at Guitar Center and landed on these because the low mid and treb frequencies really stood out on the Mackie's to me.  They have a bit of color to the sound but they are very clear and punchy. I use the Klipsch 2.1 speaker set along side the Mackie's to fill out the higher mid range sound for now.  I have the subwwoofer on the Klipsch very low for a bit of top-end lower punch but the Mackie sub is handling all the low end rumble.  Eventually I want to swap out the Klipsch set for some white-face Yamaha monitors.  Likely will do a 6 inch set to drive vocals more.</p>
+
+        <h2>Audio Interface</h2>
+        <p>I run my Mackie studio monitors, sub, and Klipsch speakers through parallel outputs on my <a href="https://www.zzounds.com/item--FOCSCAR2I4V2" target="blank">Scarlett Focusrite 2i4 Audio Interface</a>.  Volumes are:  Mackie sub at 100%, Mackie speakers each at 100% (all switches to normal), Klipsch sub at 20% (9 o'clock), Klipsch speakers at 80% (3 o'clock), Scarlett output dialed between 20% (9 o'clock) and 60% (1 o'clock)</p>
+
+        <h2>Microphone</h2>
+        <p>I use an Audio Technica AT-2020 studio condenser microphone with a telescopic boom stand, shock mount and pop filter.</p>
 
         <h2>Hub</h2>
         <p>Everything is hooked up to my Macbook Pro using a small simple <a href="https://www.amazon.com/gp/product/B071G83L1J/ref=ppx_yo_dt_b_asin_title_o04_s01?ie=UTF8&psc=1" target="blank">Anker USB C Hub</a>.</p>
 
         <h2>Computer</h2>
-        <p>I am rocking a nicely spec'd out <a href="https://www.bhphotovideo.com/bnh/controller/home?O=email&A=details&Q=&sku=1423791&is=REG" target="blank">Macbook Pro with Retina Display and Touchbar</a> that I got from B&H Photo with a $700 discount.  Woot!  2.9GHz 6‑core 8th‑generation Intel Core i9 processor (Turbo Boost up to 4.8GHz).  32GB 2400MHz DDR4 memory.  Radeon Pro 560X with 4GB of GDDR5 memory.  2TB SSD storage.</p>
+        <p>I am rocking a nicely spec'd out <a href="https://www.bhphotovideo.com/bnh/controller/home?O=email&A=details&Q=&sku=1423791&is=REG" target="blank">Macbook Pro with Retina Display and Touchbar</a> that I got from B&H Photo.  They had this model discounted $700 off and I had a credit card promo that gave me $500 cash back, with that and it being a tax-deduction I bought this obnoxiously expensive laptop for almost $1500 off retail.  Woot!</p>
+        <p style="margin-top: 20px;"><strong style="color: grey;">Specs:</strong></p>
+        <ul>
+            <li>2.9GHz 6‑Core 8th‑Gen Intel Core i9 Processor</li>
+            <li>(Turbo Boost up to 4.8GHz)</li>
+            <li>32GB 2400MHz DDR4 memory</li>
+            <li>Radeon Pro 560X with 4GB of GDDR5 memory</li>
+            <li>2TB SSD storage</li>
+        </ul>
 
         <h2>Keyboard &amp; Mouse</h2>
         <p>Sigh...   yes...  I spent a boatload of money on the space grey Apple Magic Keyboard and Magic Mouse 2.  I actually really like the mac keyboard and have gotten very used to it.  I've always been a fan of the magic mouse, especially for multiscreen switching.</p>
 
+        <h2>Printer</h2>
+        <p><a href="https://www.bestbuy.com/site/brother-hl-l2370dw-wireless-black-and-white-printer-gray/6130042.p?skuId=6130042" target="blank">Brother HL-L2370DW Wireless Black-and-White Laser Printer</a>.  Simple.  Prints stuff.</p>
+
         <h2>Phone</h2>
         <p>I have the Samsung Galaxy S10+ in Ceramic Black with 512GB internal memory.</p>
 
-        <h2>Code Editor</h2>
-        <p>I was a big Sublime Text user for a few years, wound up switching to Atom for a bit...  these days I am rocking <a href="https://code.visualstudio.com/" target="blank">Visual Studio Code</a>.  Everyone else switched to it, so I hopped on the bandwagon.  ¯\_(ツ)_/¯</p>
-
         <h2>Headphones</h2>
-        <p>I bought a set of <a href="https://www.nuraphone.com/" target="blank">Nuraphones</a> and OMG!  These things are RIDICULOUSLY AMAZING!  The bass isn't even deep, it is PROFOUND AF!  They're so clear.  It's like sitting the perfect distance from blasted studio monitors at a professional studio everytime I put them on.</p>
+        <p>I bought a set of <a href="https://www.nuraphone.com/" target="blank">Nuraphones</a> and OMG!  These things are RIDICULOUSLY AMAZING!  The bass isn't even deep, it is PROFOUND AF!  They're so clear.  It's like sitting the perfect distance from an array of blasted high-end studio monitors at a professional studio everytime I put them on.</p>
 
-        <h2>Other Apps I Use</h2>
-        <p>Gmail - I manage multiple accounts from a single inbox and it's great.</p>
-        <p>Spotify - For music.</p>
-        <p>Evernote - For taking notes.</p>
-        <p>Todoist - My preferred todo list app.</p>
-        <p>Remembear - Password Manager.</p>
-        <p>Slack - What did we do before this existed, honestly?</p>
-        <p>iTerm2 - Terminal of choice.  I use the Agnoster config found <a href="https://medium.com/swlh/power-up-your-terminal-using-oh-my-zsh-iterm2-c5a03f73a9fb" target="blank">here</a>.</p>
+        <h2>Earbuds</h2>
+        <p>I use the bluetooth (wireless) <a href="https://www.amazon.com/Samsung-Bluetooth-Wireless-Charging-Included/dp/B07MWCNR3W/ref=sr_1_3?keywords=samsung+galaxy+buds&qid=1564951557&s=gateway&sr=8-3" target="blank">Samsung Galxy Buds (Black)</a>.  These are super tiny, the charge lasts forever...  especially with the charging case they come in...  the sound quality is great.  I do wish they could go a tiny bit louder.</p>
+
+        <h1 style="color: #c8dfe6; margin-top: 100px;">Software</h1>
+
+        <h2>Mobile Stuff</h2>
+        <p>Ogio Backpack</p>
+        <p>Crumpler Messenger Bag</p>
+
+        <h2>Code Editor</h2>
+        <p>I was a big Sublime Text user for a few years, wound up switching to Atom for a bit...  these days I am rocking <a href="https://code.visualstudio.com/" target="blank">VS Code (Visual Studio Code)</a>.  Everyone else switched to it, so I hopped on the bandwagon.  ¯\_(ツ)_/¯</p>
+        <p style="margin-top: 20px;"><strong style="color: grey;">Setup:</strong></p>
+        <ul>
+            <li>Monokai Pro (Filter Spectrum) Theme</li>
+            <li>Menlo Font at 15px</li>
+        </ul>
+        <p style="margin-top: 20px;"><strong style="color: grey;">Extensions:</strong></p>
+        <ul>
+            <li>GitLens</li>
+            <li>Image Preview</li>
+            <li>Vetur</li>
+            <li>Version Lens</li>
+            <li>Prettier</li>
+            <li>PHP Namespace Resolver</li>
+            <li>PHP Intellephense</li>
+            <li>Monokai Pro</li>
+            <li>Material Icon Theme</li>
+            <li>Laravel Blade Snippets</li>
+            <li>Laravel Artisan</li>
+            <li>EditorConfig</li>
+            <li>DotENV</li>
+        </ul>
+
+        <h2>Apps I Use</h2>
+        <ul>
+            <li>Google Chrome - My preferred browser for development and general use.</li>
+            <li>Wave Plugin - For making sure websites are accessible.</li>
+            <li>Valet - Part of Laravel, I use this for my local development server.</li>
+            <li>Gmail - I manage multiple accounts from a single inbox and it's great.</li>
+            <li>Spotify - For music.</li>
+            <li>Evernote - For taking notes.</li>
+            <li>TickTick - My preferred todo list app.</li>
+            <li>Remembear - Password Manager.</li>
+            <li>TunnelBear - VPN.</li>
+            <li>VirtualBox - Use this for testing in Windows (with the Modern IE installs) and messing with Linux distributions.</li>
+            <li>Slack - What did we do before this existed, honestly?</li>
+            <li>iTerm2 - My terminal of choice.  I use OhMyZsh with the Agnoster config found <a href="https://medium.com/swlh/power-up-your-terminal-using-oh-my-zsh-iterm2-c5a03f73a9fb" target="blank">here</a>.</li>
+            <li>Homebrew - Amazing package manager for using command line and scripts to install apps on Mac (or Linux).</li>
+            <li>MacDown - Simple Markdown editor.</li>
+            <li>Skitch - Screenshot tool from the creators of Evernote.</li>
+            <li>Sip - Color picker tool.</li>
+            <li>Adobe Photoshop / Lightroom - Graphic and photo editing.</li>
+            <li>Affinity Photo - Starting to like/use this more than Photoshop.</li>
+            <li>Magnet - Window positioning app.</li>
+            <li>Presonus Studio One 4 Pro - My DAW (Digital Audio Workstation).</li>
+            <li>Line 6 Helix Native - Everything for guitar sound in one VST plugin!</li>
+            <li>Adapter - Video / Audio conversion utility.</li>
+            <li>Backblaze - Cloud backup utility.</li>
+            <li>Big Day Countdown - Large countdown reminders on my desktop.</li>
+            <li>FileZilla - For those times I need to secure FTP and am too lazy to commmand line it.</li>
+            <li>ImageOptim - For crushing images before using them on the web.</li>
+            <li>MPV - My preferred video player these days.  Though I mostly use streaming services now.</li>
+            <li>TablePlus - After using SequelPro for years, I've switched to a more supported and less-crashy tool for database management.</li>
+        </ul>
 
     </div>
 </template>
