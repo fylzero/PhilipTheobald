fd75a3808f54efc778895b3a7562dbccd72cf487
Philip Theobald <fylzero@gmail.com>
2019-11-18 23:19:33 -0600

    Set up Envoy deployment script

---
+++ Envoy.blade.php
@@ -0,0 +1,16 @@
+@servers(['localhost' => '127.0.0.1', 'www' => ['serverpilot@104.236.117.4']])
+
+{{-- envoy run deploy --}}
+@task('deploy', ['on' => 'www'])
+    cd ~/apps/philip-theobald
+    php artisan down --message="Deploying New Code"
+    git pull origin master
+    composer install --optimize-autoloader --no-dev
+    php artisan config:clear
+    {{-- php artisan config:cache --}}
+    php artisan route:clear
+    {{-- php artisan route:cache --}}
+    npm install --production
+    {{-- php artisan migrate --force --}}
+    php artisan up
+@endtask
