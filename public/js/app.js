1928715fa82db5bb17b54bfe526317535c1ed0f0
Philip Theobald <ptheobald@neoncrm.com>
2020-01-13 14:59:50 -0600

    Added site image metadata

--- public/js/app.js
+++ public/js/app.js
@@ -33710,7 +33710,7 @@ var staticRenderFns = [
         _c("li", [
           _vm._v("Laravel Valet - "),
           _c("span", { staticClass: "little" }, [
-            _vm._v("Best way to server sites locally imho.")
+            _vm._v("Best way to serve sites locally imho.")
           ])
         ]),
         _vm._v(" "),
@@ -75741,8 +75741,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/fyl/Google Drive/_code/_valet/PhilipTheobald/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/fyl/Google Drive/_code/_valet/PhilipTheobald/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/philiptheobald/Code/Valet/PhilipTheobald/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/philiptheobald/Code/Valet/PhilipTheobald/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="theme-color" content="#222222" />
+    <meta property="og:title" content="Philip Theobald">
+    <meta property="og:image" content="https://www.philiptheobald.com/img/jpg/fyl.jpg">
+    <meta property="og:logo" content="https://www.philiptheobald.com/img/jpg/fyl.jpg" size="200x200">
     <title>{{ env('APP_NAME') }}</title>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' rel="stylesheet">
