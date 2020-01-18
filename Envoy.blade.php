d1d37952c6131be95bb58e0b4644871680e88103
Philip Theobald <fylzero@gmail.com>
2020-01-18 10:04:05 -0600

    Updated envoy script to build prod locally

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,15 +1,23 @@
 @servers(['localhost' => '127.0.0.1', 'www' => ['serverpilot@104.236.117.4']])
 
-{{-- envoy run deploy --}}
-@task('deploy', ['on' => 'www'])
+@story('deploy')
+    build
+    deployment
+@endstory
+
+@task('build', ['on' => 'localhost'])
+    npm run prod
+@endtask
+
+@task('deployment', ['on' => 'www'])
     cd ~/apps/philip-theobald
     php artisan down --message="Deploying New Code"
     git pull origin master
     composer install --optimize-autoloader --no-dev
     php artisan config:clear
-    {{-- php artisan config:cache --}}
+    php artisan config:cache
     php artisan route:clear
-    {{-- php artisan route:cache --}}
+    php artisan route:cache
     npm install --production
     {{-- php artisan migrate --force --}}
     php artisan up
