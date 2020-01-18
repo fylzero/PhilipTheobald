afd80863929caf64df29dd7768b73b645985f955
Philip Theobald <fylzero@gmail.com>
2020-01-18 10:59:07 -0600

    Added dynamic page titles for Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,36 +1,98 @@
-window._ = require('lodash');
+window._ = require("lodash");
 
-window.axios = require('axios');
+window.axios = require("axios");
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+    window.axios.defaults.headers.common["X-CSRF-TOKEN"] = token.content;
 } else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+    console.error(
+        "CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token"
+    );
 }
 
-import Vue from 'vue';
-import Routes from './routes';
-import VueRouter from 'vue-router';
-import Vuetify from 'vuetify';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faLightbulb, faMoon } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import Vue from "vue";
+import Routes from "./routes";
+import VueRouter from "vue-router";
+import Vuetify from "vuetify";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faLightbulb, faMoon } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 library.add(faLightbulb, faMoon);
-Vue.component('font-awesome-icon', FontAwesomeIcon);
-
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.use(VueRouter);
 Vue.use(Vuetify);
 
-const files = require.context('./', true, /\.vue$/i)
-files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+const files = require.context("./", true, /\.vue$/i);
+files.keys().map(key =>
+    Vue.component(
+        key
+            .split("/")
+            .pop()
+            .split(".")[0],
+        files(key).default
+    )
+);
+
+const router = new VueRouter(Routes);
+
+
+// This callback runs before every route change, including on page load.
+router.beforeEach((to, from, next) => {
+    // This goes through the matched routes from last to first, finding the closest route with a title.
+    // eg. if we have /some/deep/nested/route and /some, /deep, and /nested have titles, nested's will be chosen.
+    const nearestWithTitle = to.matched
+        .slice()
+        .reverse()
+        .find(r => r.meta && r.meta.title);
+
+    // Find the nearest route element with meta tags.
+    const nearestWithMeta = to.matched
+        .slice()
+        .reverse()
+        .find(r => r.meta && r.meta.metaTags);
+    const previousNearestWithMeta = from.matched
+        .slice()
+        .reverse()
+        .find(r => r.meta && r.meta.metaTags);
+
+    // If a route with a title was found, set the document (page) title to that value.
+    if (nearestWithTitle) document.title = nearestWithTitle.meta.title;
+
+    // Remove any stale meta tags from the document using the key attribute we set below.
+    Array.from(
+        document.querySelectorAll("[data-vue-router-controlled]")
+    ).map(el => el.parentNode.removeChild(el));
+
+    // Skip rendering meta tags if there are none.
+    if (!nearestWithMeta) return next();
+
+    // Turn the meta tag definitions into actual elements in the head.
+    nearestWithMeta.meta.metaTags
+        .map(tagDef => {
+            const tag = document.createElement("meta");
+
+            Object.keys(tagDef).forEach(key => {
+                tag.setAttribute(key, tagDef[key]);
+            });
+
+            // We use this to track which meta tags we create, so we don't interfere with other ones.
+            tag.setAttribute("data-vue-router-controlled", "");
+
+            return tag;
+        })
+        // Add the meta tags to the document head.
+        .forEach(tag => document.head.appendChild(tag));
+
+    next();
+});
 
 const app = new Vue({
-    el: '#app',
-    router: new VueRouter(Routes)
+    el: "#app",
+    router
 });
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,23 +1,21 @@
-import Home from './components/Home';
-import About from './components/About';
-import Uses from './components/Uses';
-import Skills from './components/Skills';
-import Hobbies from './components/Hobbies';
-import Companies from './components/Companies';
-import Contact from './components/Contact';
+import Home from "./components/Home";
+import About from "./components/About";
+import Uses from "./components/Uses";
+import Skills from "./components/Skills";
+import Hobbies from "./components/Hobbies";
+import Companies from "./components/Companies";
+import Contact from "./components/Contact";
 
 export default {
-
     mode: 'history',
     routes: [
         // { path: '*', component: NotFound },
-        { path: '/', component: Home },
-        { path: '/about', component: About },
-        { path: '/uses', component: Uses },
-        { path: '/skills', component: Skills },
-        { path: '/hobbies', component: Hobbies },
-        { path: '/companies', component: Companies },
-        { path: '/contact', component: Contact }
+        { path: "/", component: Home, meta: { title: 'Philip Theobald' } },
+        { path: "/about", component: About, meta: { title: 'About | Philip Theobald' } },
+        { path: "/uses", component: Uses, meta: { title: 'Uses | Philip Theobald' } },
+        { path: "/skills", component: Skills, meta: { title: 'Skills | Philip Theobald' } },
+        { path: "/hobbies", component: Hobbies, meta: { title: 'Hobbies | Philip Theobald' } },
+        { path: "/companies", component: Companies, meta: { title: 'Companies | Philip Theobald' } },
+        { path: "/contact", component: Contact, meta: { title: 'Contact | Philip Theobald' } }
     ]
-
 };
