165f8c28405864fffc4d94be391d4bd5f0b28915
Philip Theobald <fylzero@gmail.com>
2020-02-12 12:41:37 -0600

    Updated deploy script

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,18 +1,6 @@
 @servers(['localhost' => '127.0.0.1', 'www' => ['serverpilot@104.236.117.4']])
 
-@story('deploy')
-    build
-    deployment
-@endstory
-
-@task('build', ['on' => 'localhost'])
-    npm run prod
-    git add .
-    git commit -m 'Ran npm run prod'
-    git push
-@endtask
-
-@task('deployment', ['on' => 'www'])
+@task('deploy', ['on' => 'www'])
     cd ~/apps/philip-theobald
     php artisan down --message="Deploying New Code"
     git pull origin master
@@ -21,8 +9,8 @@
     php artisan config:cache
     php artisan route:clear
     php artisan route:cache
-    {{-- npm install --production --}}
     npm ci
+    npm run production
     {{-- php artisan migrate --force --}}
     php artisan up
 @endtask
